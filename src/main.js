1ac115173e600596988e6bd861f64edbb37d7c44
Luu Danh Nhan <nhan.luu@icomm.vn>
2021-02-22 00:11:25 +0700

    update code

--- src/main.js
+++ src/main.js
@@ -1,8 +1,32 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import Vuex from "vuex";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import VueRouter from "vue-router";
 
-Vue.config.productionTip = false
+Vue.use(Vuex);
+Vue.use(ElementUI);
+Vue.use(VueRouter);
+
+Vue.config.productionTip = false;
+
+const store = new Vuex.Store({
+  state: {
+    count: 0,
+  },
+  mutations: {
+    increment(state) {
+      state.count++;
+    },
+  },
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+  store,
+}).$mount("#app");
+
+// store.commit("increment");
+
+// console.log(store.state.count); // -> 1
